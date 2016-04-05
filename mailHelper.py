fe4d32bb71d1dec5bc5e3b9ead920d2f763e6e6f
onefor1 <332861960@qq.com>
2016-04-05 10:09:53 +0800

    Update mailHelper.py

--- mailHelper.py
+++ mailHelper.py
@@ -29,8 +29,12 @@ def guess_charset(msg):
 #indent用于缩进显示
 def download_attachment(msg, indent = 0):
 	#先在当前目录创建保存本次作业的子目录
-	if os.path.exists(job_number) == False:
-		os.mkdir(job_number)
+	if job_number:
+		if os.path.exists(job_number) == False:
+			os.mkdir(job_number)
+	else:
+		if os.path.exists("temp") == False:
+			os.mkdir("temp")
 
 	#若indent不等于0，则表示该部分不包含邮件头
 	if indent == 0:
@@ -46,9 +50,13 @@ def download_attachment(msg, indent = 0):
 			subject = subject.split('_')
 		if 3 != len(subject): #邮件主题格式不对就返回
 			return
-		times, student_number, student_name = subject
-		if times != job_number: #只下载指定的作业
-			return
+		times, s_number, s_name = subject
+		if job_number:
+			if times != job_number: #只下载指定的作业
+				return
+		if student_number:
+			if s_number != student_number: #只下载指定的作业
+				return
 		
 	if(msg.is_multipart()):
 		#如果邮件对象是一个MIMEMultipart，
@@ -66,19 +74,27 @@ def download_attachment(msg, indent = 0):
 			filename = decode_str(msg.get_filename())
 			#filename = msg.get_filename()
 			print filename
-			f = open("%s/%s" % (job_number, filename), 'wb')
+			if job_number:
+				f = open("%s/%s" % (job_number, filename), 'wb')
+			else:
+				f = open("temp/%s" % filename, 'wb')
 			f.write(base64.decodestring(msg.get_payload()))
 			f.close()
 
-def analysis_mail():
+def analysis_mail(select):
 	global job_number
 	global student_number
-	global student_name
-	job_number = raw_input("please input the job_number: ")
+	job_number = ''
+	student_number = ''
+	
+	if select == "1":
+		job_number = raw_input("please input the job number: ")
+	else select =="2":
+		student_number = raw_input("please input the student number: ")
+	
 
-	email = 'your_email'
-	#注意,这里填写的是授权密码,不是登录密码
-	password = 'your_pass'
+	email = 'javahomework2016'
+	password = '1q2w3e'
 	pop3_server = 'pop.126.com'
 
 	#connect to pop3 server
@@ -108,6 +124,21 @@ def analysis_mail():
 
 	server.quit()
 
+def print_menu():
+	print "Menu:"
+	print "1.download the whole homework by job number"
+	print "2.download specify homework by student number"
+	print "3.download all the homework"
+	print "4.exit"
+	select = raw_input("")
+
 if __name__ == "__main__":
-	analysis_mail()
+	print_menu()
+	while 1:
+		if select == "3":
+			break
+		else:
+			analysis_mail(select)
+		
+		print_menu()
 	
